196ab14135d822822dcdc75926e28fa06cc3464b
felprangel <feliperangel897@gmail.com>
2025-11-08 20:07:43 -0300

    adiciona classe de conexão com o banco

---
+++ src/main/java/com/projetoa3/projetoa3/ConexaoBanco.java
@@ -0,0 +1,20 @@
+package com.projetoa3.projetoa3;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConexaoBanco {
+    private static final String URL = "jdbc:mysql://localhost:3306/todo-app";
+    private static final String USER = "root";
+    private static final String PASSWORD = "";
+
+    public static Connection getConnection() throws SQLException {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            return DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (ClassNotFoundException e) {
+            throw new SQLException("Driver do MariaDB não encontrado", e);
+        }
+    }
+}
