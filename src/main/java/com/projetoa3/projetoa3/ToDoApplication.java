c00258423790b2f62b7622ea9306d2ba13786ef9
felprangel <feliperangel897@gmail.com>
2025-11-08 20:26:32 -0300

    ajusta classe de aplicação

--- src/main/java/com/projetoa3/projetoa3/ToDoApplication.java
+++ src/main/java/com/projetoa3/projetoa3/ToDoApplication.java
@@ -7,10 +7,11 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class ToDoApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(ToDoApplication.class.getResource("ToDo.fxml"));
+        // TODO: verificar melhor tamanho pra janela
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Hello!");
         stage.setScene(scene);
