c4dd3224ccc3c33169b138c05eb53a77ee8819e8
felprangel <feliperangel897@gmail.com>
2025-11-08 20:24:59 -0300

    adiciona função de incrementar o número de tarefas

--- src/main/java/com/projetoa3/projetoa3/ToDoController.java
+++ src/main/java/com/projetoa3/projetoa3/ToDoController.java
@@ -1,4 +1,18 @@
 package com.projetoa3.projetoa3;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
 public class ToDoController {
+
+    private int counter;
+
+    @FXML
+    private Label taskCount;
+
+    @FXML
+    protected void addTask() {
+        counter++;
+        taskCount.setText(String.valueOf(counter));
+    }
 }
