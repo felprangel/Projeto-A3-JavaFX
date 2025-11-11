11b92f39347bfb30bf4e86bb60e17ae421523691
felprangel <feliperangel897@gmail.com>
2025-11-11 18:53:23 -0300

    altera driver chamado

--- src/main/java/com/projetoa3/projetoa3/DatabaseConnection.java
+++ src/main/java/com/projetoa3/projetoa3/DatabaseConnection.java
@@ -11,7 +11,7 @@ public class DatabaseConnection {
 
     public static Connection getConnection() throws SQLException {
         try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
+            Class.forName("com.mariadb.jdbc.Driver");
             return DriverManager.getConnection(URL, USER, PASSWORD);
         } catch (ClassNotFoundException e) {
             throw new SQLException("Driver do MariaDB não encontrado", e);
