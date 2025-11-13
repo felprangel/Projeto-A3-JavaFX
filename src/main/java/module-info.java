ee8d32132d56cb427e10f94dd02651df1a93e5fa
felprangel <feliperangel897@gmail.com>
2025-11-12 22:43:34 -0300

    altera string da classe utilizada

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,6 +5,7 @@ module com.projetoa3.projetoa3 {
     requires org.controlsfx.controls;
     requires org.kordamp.bootstrapfx.core;
     requires java.sql;
+    requires mysql.connector.j;
 
     opens com.projetoa3.projetoa3 to javafx.fxml;
     exports com.projetoa3.projetoa3;
