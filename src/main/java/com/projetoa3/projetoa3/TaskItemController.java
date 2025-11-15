6e16c27c10d4445ee681a237a153b19016ca3710
felprangel <feliperangel897@gmail.com>
2025-11-15 20:02:40 -0300

    remove import não utilizado

--- src/main/java/com/projetoa3/projetoa3/TaskItemController.java
+++ src/main/java/com/projetoa3/projetoa3/TaskItemController.java
@@ -5,7 +5,6 @@ import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
-import java.time.LocalDateTime;
 
 public class TaskItemController {
 
